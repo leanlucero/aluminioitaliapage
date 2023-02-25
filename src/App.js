ec524c1aac73d818b5202492e3cc8e9ec932c322
Leandro <leandrolucero@outlook.com>
2023-02-24 22:50:20 -0300

    problema login router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { lazy } from 'react';
 import "./App.css"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import Index from './Contenido/Index.js'
 import Login from './Contenido/Login.js'
 import Abmtrabajos from './Contenido/ABMtrabajos/Abmtrabajos'
@@ -12,18 +12,15 @@ const Abmcategorias = lazy(() => import('./Contenido/ABMcategorias/Abmcategorias
 function App() {
   return (
     <>
-      <BrowserRouter>
         <LoginProvider>
           <Menu />
-          <Routes>
-            <Route exact path="/aluminioitaliapage" element={<Index />} />
+          <Routes  >
+            <Route exact path="/aluminioitaliapage/" element={<Index />} />
             <Route exact path="/aluminioitaliapage/login" element={<Login />} />
             <Route exact path="/aluminioitaliapage/admin/trabajos" element={<Abmtrabajos />} />
             <Route exact path="/aluminioitaliapage/admin/categorias" element={<Abmcategorias />} />
-            <Route path="*" element={<Index />} />
           </Routes>
         </LoginProvider>
-      </BrowserRouter>
     </>
   )
 }
