b6ff42da084f503272acd5f9d964741a92bea8b7
Leandro <leandrolucero@outlook.com>
2023-05-14 23:41:17 -0300

    url cambiada por una funcionando en render

--- src/Contenido/Service/Conexiones.js
+++ src/Contenido/Service/Conexiones.js
@@ -1,6 +1,6 @@
 import axios from 'axios'; 
 
-const URL = `http://localhost:8080`
+const URL = `https://apirestaluminioitalia.onrender.com`
 
  export const logearse = async (usuario , pass) =>{
   let aux
